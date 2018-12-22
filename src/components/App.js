59940b4ff6f2080be7954ca495ddd11c3e74221b
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2018-12-23 10:53:44 +1100

    Replace Promise with async-await method

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,19 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: {
-          query: term
-        },
-        headers: {
-          Authorization:
-            "Client-ID bc2a7c24c3eabacbe6554246829d90be1b6b3e11cf271559f643a53f626e602e"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
-  };
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID bc2a7c24c3eabacbe6554246829d90be1b6b3e11cf271559f643a53f626e602e"
+      }
+    });
+
+    console.log(response.data.results);
+  }
 
   render() {
     return (
